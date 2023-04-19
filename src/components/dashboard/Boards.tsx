c0c4d3010ddfda0de9791daed12047c814f38a20
James <jamesyj.hong@gmail.com>
2023-04-19 00:18:23 -0400

    feat: update boards and thumbnails (wip)

--- src/components/dashboard/Boards.tsx
+++ src/components/dashboard/Boards.tsx
@@ -1,21 +1,52 @@
+import { useState } from "react";
+import { FaStar } from "react-icons/fa";
+import { TbNewSection } from "react-icons/tb";
+import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { createBoard } from "../../redux/slices/boards";
 import { RootState } from "../../redux/store";
-import { SectionContainer, ThumbnailGrid, SectionTitle } from "./styles";
+import { Button, ButtonContainer } from "../common/Button";
+import Icon from "../common/Icon";
+import { SectionContainer, ThumbnailGrid, SectionTitle, SectionHeader } from "./styles";
 import Thumbnail from "./Thumbnail";
 
 const Boards = () => {
-  const { items } = useSelector((state: RootState) => state.boards);
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+	const { boards } = useSelector((state: RootState) => state.boards);
 
-  return (
-    <SectionContainer>
-      <SectionTitle>Boards</SectionTitle>
-      <ThumbnailGrid>
-        {items.map((item, index) => (
-          <Thumbnail key={index} {...item} />
-        ))}
-      </ThumbnailGrid>
-    </SectionContainer>
-  );
+	const handleAddMemo = () => {
+		console.log("add new board!");
+		dispatch(createBoard());
+	};
+
+	const handleViewFavoriteBoards = () => {
+		console.log("view favorite boards!");
+	};
+	return (
+		<SectionContainer>
+			<SectionHeader>
+				<ButtonContainer>
+					<Button onClick={handleAddMemo}>
+						<Icon element={<TbNewSection size={25} />} />
+					</Button>
+					<Button onClick={handleViewFavoriteBoards}>
+						<Icon element={<FaStar size={25} />} />
+					</Button>
+				</ButtonContainer>
+			</SectionHeader>
+			<ThumbnailGrid>
+				{Object.keys(boards).map((key) => {
+					return (
+						<div key={key} onClick={() => navigate(`/boards/${key}`)}>
+							<Thumbnail key={key} id={key} {...boards[key]} />
+						</div>
+					);
+				})}
+			</ThumbnailGrid>
+		</SectionContainer>
+	);
 };
 
 export default Boards;
--- src/components/dashboard/Thumbnail.tsx
+++ src/components/dashboard/Thumbnail.tsx
@@ -1,40 +1,74 @@
+import { FaRegTrashAlt, FaStar } from "react-icons/fa";
+import { useDispatch } from "react-redux";
 import styled from "styled-components";
+import { removeBoard } from "../../redux/slices/boards";
+import { Button, ButtonContainer } from "../common/Button";
+import { ActionsContainer } from "./styles";
 
-const Thumbnail = ({ title, image }) => {
-  return (
-    <ThumbnailContainer style={{ backgroundImage: `url(${image})` }}>
-      <Title>{title}</Title>
-    </ThumbnailContainer>
-  );
+const Thumbnail = ({ id, name, image, isEditing }) => {
+	const dispatch = useDispatch();
+
+	const handleDeleteBoard = () => {
+		dispatch(removeBoard({ boardId: id }));
+	};
+	return (
+		<ThumbnailContainer isEditing={isEditing} image={image}>
+			<ActionsContainer onClick={(e) => e.stopPropagation()}>
+				<FaRegTrashAlt id="delete" onClick={handleDeleteBoard} size={20} />
+				<FaStar id="favorite" onClick={() => console.log("add to favorites!")} size={20} />
+			</ActionsContainer>
+			<Overlay />
+			<Title>{name}</Title>
+		</ThumbnailContainer>
+	);
 };
 
 export default Thumbnail;
 
 const ThumbnailContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-end;
-  padding: 1rem;
-  color: white;
-  transition: all 0.3s ease;
-  background-position: center;
-  background-size: cover;
-  width: 8.75rem;
-  height: 8.75rem;
+	position: relative;
+	background: url(${(props) => props.image});
+	background-size: cover;
+	background-repeat: no-repeat;
+	display: flex;
+	padding: 1.5rem;
+	color: #fefefe;
+	transition: all 0.3s ease;
+	width: 10.75rem;
+	height: 8.75rem;
+	border-radius: 1rem;
+	cursor: pointer;
+	box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
+	${(props) => props.isEditing && `border: 2px solid #007bff;`}
 
-  @media (min-width: 768px) {
-    width: 12rem;
-    height: 12rem;
-  }
+	@media (min-width: 768px) {
+		width: 18rem;
+		height: 10rem;
+	}
 
-  &:hover {
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
-    transform: translateY(-5px);
-  }
+	&:hover {
+		box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+		transform: translateY(-5px);
+	}
+`;
+
+const Overlay = styled.div`
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	background-color: rgba(0, 0, 0, 0.6); /* Change the alpha value to adjust the darkness */
+	border-radius: 1rem;
+	transition: all 0.3s ease;
 `;
 
 const Title = styled.h3`
-  margin-bottom: 0.5rem;
-  font-size: 1.125rem;
-  font-weight: bold;
+	position: absolute;
+	right: 20px;
+	bottom: 10px;
+	margin-bottom: 0.5rem;
+	font-size: 1.25rem;
+	font-weight: bold;
+	z-index: 999;
 `;
