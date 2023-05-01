5f215cf5fa141cdf2c3ffcf1be9ee10ac8b12e11
James <jamesyj.hong@gmail.com>
2023-04-30 20:25:01 -0400

    feat: add layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,25 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import "./global.css";
+import "./globalThis.css";
 
 // pages
 import Dashboard from "./pages/dashboard";
 import Board from "./pages/board";
-import Memo from "./pages/Memo";
-import NotFound from "./pages/NotFound";
+import Memo from "./pages/memo";
+import NotFound from "./pages/not-found";
 
-// components
-import Boards from "./components/dashboard/Boards";
-import Memos from "./components/dashboard/Memos";
+// layout
+import Layout from "./layout";
 
 export const App = () => {
 	return (
-		<>
+		<Layout>
 			<Routes>
 				<Route path="/" element={<Dashboard />} />
-				<Route path="/boards" element={<Boards />} />
 				<Route path="/boards/:id" element={<Board />} />
-				<Route path="/memos" element={<Memos />} />
 				<Route path="/memos/:id" element={<Memo />} />
 				<Route path="/not-found" element={<NotFound />} />
-				<Route path="*" element={<Navigate to="/not-found" />} />
+				<Route path="*" element={<Navigate to="/" />} />
 			</Routes>
-		</>
+		</Layout>
 	);
 };
