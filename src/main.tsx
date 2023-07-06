d3d40fa5e481cf0beb4af706703eb66a815b68a7
James <jamesyj.hong@gmail.com>
2023-07-05 22:46:23 -0400

    feat: add tailwindcss and update packages

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,22 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider as ReduxProvider } from "react-redux";
 import { persistor, store } from "./redux/store";
 import { App } from "./App";
 import { PersistGate } from "redux-persist/integration/react";
 
-const container = document.getElementById("root") as HTMLElement;
+import "./app.css";
+import "./tailwind.css";
 
-const root = createRoot(container);
-
-root.render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<ReduxProvider store={store}>
-				<PersistGate loading={null} persistor={persistor}>
-					<App />
-				</PersistGate>
-			</ReduxProvider>
-		</BrowserRouter>
-	</React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ReduxProvider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </ReduxProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
 );
